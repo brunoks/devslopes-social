cc806df16bffd4238c81364c43021571fa1b62e3
brunoks <brunokasv@hotmail.com>
2017-10-24 15:26:13 -0200

    first commit

---
+++ devslopes-social/devslopes-social/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  devslopes-social
+//
+//  Created by Bruno Vieira on 24/10/17.
+//  Copyright © 2017 Bruno Vieira. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
